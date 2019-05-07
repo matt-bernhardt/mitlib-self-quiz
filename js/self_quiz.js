ffeee4a85878159af0db7ef447b5f4aa966bdb81
Matt Bernhardt <mjbernha@mit.edu>
2019-05-07 18:42:31 -0400

    Adds settings page and relevant infrastructure

---
+++ js/self_quiz.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Contains js for the accordion example.
+ */
+
+(function ($) {
+
+  'use strict';
+
+  console.log('self_quiz.js is ready...');
+})(jQuery);
---
+++ src/Controller/QuizController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\mitlib_self_quiz\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines HelloController class.
+ */
+class QuizController extends ControllerBase {
+
+	/**
+	 * Display the markup.
+	 *
+	 * @return array
+	 *   Return markup array.
+	 */
+	public function settings() {
+		return [
+			'#type' => 'markup',
+			'#markup' => $this->t('Hello, Quiz!')
+		];
+	}
+}
